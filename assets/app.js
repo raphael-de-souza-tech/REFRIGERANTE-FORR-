a62aef703eb85d37cfbe328abb2f8968a24443b7
raphael de souza <raphaelcostay@gmail.com>
2025-11-09 23:53:36 -0300

    adicionando efeito na garrafa maior com js

---
+++ assets/app.js
@@ -0,0 +1,17 @@
+// const refriLogo = document.querySelector(".refri-logo");
+// const refriContainer = document.querySelector(".refri-container");
+
+// refriLogo.addEventListener('click', () => {
+//     refriContainer.classList.toggle("active-refri");
+// });
+const refriLogo = document.querySelector(".refri-logo");
+const refriLogoDois = document.querySelector(".refri-logo-02");
+const refriContainer = document.querySelector(".refri-container");
+
+refriLogo.addEventListener('click', ()=>{
+    refriContainer.classList.toggle("vibrando");
+});
+
+refriLogoDois.addEventListener('click', ()=>{
+    refriContainer.classList.toggle("vibrando");
+});
