a0954eca52ab6e10fa1b02d6e86d75b6d92e2533
raphael de souza <raphaelcostay@gmail.com>
2025-12-02 04:16:11 -0300

    mudança completa no projeto

--- assets/app.js
+++ assets/app.js
@@ -4,14 +4,14 @@
 // refriLogo.addEventListener('click', () => {
 //     refriContainer.classList.toggle("active-refri");
 // });
-const refriLogo = document.querySelector(".refri-logo");
-const refriLogoDois = document.querySelector(".refri-logo-02");
-const refriContainer = document.querySelector(".refri-container");
+// const refriLogo = document.querySelector(".refri-logo");
+// const refriLogoDois = document.querySelector(".refri-logo-02");
+// const refriContainer = document.querySelector(".refri-container");
 
-refriLogo.addEventListener('click', ()=>{
-    refriContainer.classList.toggle("vibrando");
-});
+// refriLogo.addEventListener('click', ()=>{
+//     refriContainer.classList.toggle("vibrando");
+// });
 
-refriLogoDois.addEventListener('click', ()=>{
-    refriContainer.classList.toggle("vibrando");
-});
+// refriLogoDois.addEventListener('click', ()=>{
+//     refriContainer.classList.toggle("vibrando");
+// });
